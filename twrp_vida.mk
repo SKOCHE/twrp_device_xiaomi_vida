1ee78da1ffe1fe867e0aabb9a605ca3da1c9e009
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2024-02-06 10:51:28 +0700

    Add files via upload

--- twrp_vida.mk
+++ twrp_vida.mk
@@ -1,29 +1,32 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The TWRP Open Source Project
+# Copyright (C) 2021 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from vida device
 $(call inherit-product, device/xiaomi/vida/device.mk)
 
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/pb/config/common.mk)
+
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vida
 PRODUCT_NAME := twrp_vida
-PRODUCT_BRAND := Redmi
+PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := vida
-PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-
-    PRIVATE_BUILD_DESC="vida-user 12 SP1A.210812.016 V14.0.4.0.TGDINXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/vida_in/vida:12/SP1A.210812.016/V14.0.4.0.TGDINXM:user/release-keys
+PRODUCT_MANUFACTURER := xiaomi
